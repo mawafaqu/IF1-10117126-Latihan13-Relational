309492366eb450bffa9d267bd971843d14b43592
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:49:30 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan13-Relational/src/if1/pkg10117126/latihan13/relational/IF110117126Latihan13Relational.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan13.relational;
+
+
+public class IF110117126Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
